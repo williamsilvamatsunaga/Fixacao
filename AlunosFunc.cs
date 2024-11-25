29854d17ff48f44beab85cc0b0c36684653f6909
William Matsunaga <williamsilvamatsunaga@outlook.com>
2024-11-24 21:41:32 -0400

    Fixacao

---
+++ AlunosFunc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class AlunosFunc
+{
+    public List<Aluno> alunosList = new List<Aluno>();
+
+    public void AdicionarAlunos(Aluno a)
+    {
+        alunosList.Add(a);
+    }
+
+    public void Listar()
+    {
+        foreach (var aluno in alunosList)
+        {
+            Console.WriteLine($"Nome: {aluno.nome}, idade: {aluno.nome}, {aluno.serie}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Xml;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        AlunosFunc aFunc = new AlunosFunc();
+
+        do
+        {
+            
+
+            int opcao = 0;
+            Console.Write("1 p/ ADD A LISTA 2 p/ add LISTAR");
+            opcao = int.Parse(Console.ReadLine());
+
+            if (opcao == 1)
+            {
+                Aluno a = new Aluno();
+
+                Console.Write("Nome:");
+                a.nome = Console.ReadLine();
+
+                Console.Write("Idade: ");
+                a.idade = Console.ReadLine();
+
+                Console.Write("Série: ");
+                a.serie = Console.ReadLine();
+
+                aFunc.AdicionarAlunos(a);
+            }
+
+            if (opcao == 2)
+            {
+                aFunc.Listar();
+            }
+
+        } while (true);     
+    }
+}
+
+
+
